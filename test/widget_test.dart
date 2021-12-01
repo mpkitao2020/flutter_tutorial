c4585ada411447d1366b86efd72da81609fde5b3
hkitao <h.kitao@magic-plus.net>
2021-12-01 11:17:22 +0900

    アイコン変更、テスト

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flatter_tutorial/main.dart';
+import 'package:flatter_tutorial/hello_world/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:flatter_tutorial/hello_world/main.dart' as app;
+// import 'package:holl_workd/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
+}
